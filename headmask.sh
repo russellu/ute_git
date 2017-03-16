2b7995db7f31c806f117f91f0588c8cb6482ed6d
Russell Butler <russell.buttler@usherbrooke.ca>
2017-03-16 11:30:50 -0400

    Update headmask.sh

--- headmask.sh
+++ headmask.sh
@@ -3,6 +3,6 @@
 flirt -in res_ute.nii.gz -ref /media/sf_shared/regute2/ref_ute.nii.gz -dof 12 -omat ute_2_template.m -out ute_affine.nii.gz
 fnirt --ref=ref_ute.nii.gz --in=ute_affine.nii.gz --iout=warped.nii.gz --fout=field.nii.gz --cout=coeffs.nii.gz --regmod=membrane_energy 
 invwarp -w field.nii.gz -o inv_field.nii.gz -r ute_affine.nii.gz
-applywarp -i /media/sf_shared/regute2/mask2.nii.gz -r ./ute_affine.nii.gz -o mask_affine.nii.gz -w inv_field.nii.gz --interp=nn
+applywarp -i /media/sf_shared/regute2/finalmask.nii.gz -r ./ute_affine.nii.gz -o mask_affine.nii.gz -w inv_field.nii.gz --interp=nn
 convert_xfm -omat inv_affine.m -inverse ute_2_template.m
 flirt -in mask_affine.nii.gz -ref res_ute.nii.gz -applyxfm -init inv_affine.m -inter nearestneighbour -out mask.nii.gz
